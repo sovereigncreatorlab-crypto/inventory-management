8e0458e88206aeaa98841dda3755449c70b932f6
sovereigncreatorlab-crypto <sovereigncreatorlab@gmail.com>
2025-10-20 19:24:10 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
-const handler = NextAuth(authOptions)
+// Add secret here as well
+const handler = NextAuth({
+  ...authOptions,
+  secret: process.env.NEXTAUTH_SECRET,
+})
+
 export { handler as GET, handler as POST }
